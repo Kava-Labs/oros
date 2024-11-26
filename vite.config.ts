e94729827d6257db95fa240e4c6fac9079bbb3df
samcode206 <hadi.sameem.dev@gmail.com>
2024-11-26 12:00:28 -0800

    fix: rename vitest setUp file for clarity

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   test: {
     environment: "jsdom",
-    setupFiles: './setup.js',
+    setupFiles: './vitestSetup.ts',
     exclude: [...configDefaults.exclude, "e2e"],
   }
 })
