775d8c848ee76e1d8974a9faa71109113922b968
samcode206 <hadi.sameem.dev@gmail.com>
2024-11-26 10:13:33 -0800

    feat: Add vitest dependencies
    add vitest dependencies along with react specifc test libraries, add a setup.js file for vitest, make sure to ignore e2e directory so vitest doesn't run playwright tests (playwright set up coming soon)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: './setup.js',
+    exclude: [...configDefaults.exclude, "e2e"],
+  }
 })
