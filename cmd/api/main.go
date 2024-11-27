06d485241b32fa39be52de9ef50297d4f4b6b9bc
Nick DeLuca <nickdeluca08@gmail.com>
2024-11-27 16:51:12 +0000

    feat: Add basic server with e2e test capabilities
    This adds a server that used the KAVACHAT_API_PORT variable to determine
    what port to listen too.  If this variable is set to 0, then the server
    will listen on any avaliable port.  If this variable is unset, the
    server will listen on port 5555.
    
    This also adds a healthcheck endpoint and the start of an e2e setup
    that starts the server with KAVACHAT_API_PORT=0, gets the listening port
    from the stdout, and polls the healthcheck until the server is ready.
    
    If the server does not start within the process context deadline, the
    process and running go-routines are cleaned up appropiatly.  In
    addition, un-recoverable errors are caught and causes the server to
    shutdown.
    
    This test will form the basis for e2e testing openai proxy handling,
    in addition allow for multiple e2e tests to run in parallel without
    sharing state.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"log/slog"
+	"net"
+	"net/http"
 	"os"
+	"strconv"
+)
+
+const (
+	defaultAPIPort = 5555
 )
 
 var ErrOpenAIKeyRequired = errors.New("OPENAI_API_KEY is required")
@@ -17,6 +25,38 @@ func main() {
 	}
 
 	logger.Info("Starting Kavachat API!")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "available")
+	})
+
+	port := defaultAPIPort
+	if envPort := os.Getenv("KAVACHAT_API_PORT"); envPort != "" {
+		parsedPort, err := strconv.Atoi(envPort)
+		if err != nil {
+			logFatal(logger, err)
+		}
+		port = parsedPort
+	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		logFatal(logger, err)
+	}
+	tcpAddr := listener.Addr().(*net.TCPAddr)
+	logger.Info("listening", "port", tcpAddr.Port)
+
+	server := &http.Server{
+		Handler:  mux,
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("serving", "addr", listener.Addr())
+
+	if err := server.Serve(listener); err != http.ErrServerClosed {
+		logFatal(logger, err)
+	}
 }
 
 func logFatal(logger *slog.Logger, err error) {
--- cmd/api/main_test.go
+++ cmd/api/main_test.go
@@ -1,12 +1,20 @@
 package main_test
 
 import (
+	"bufio"
 	"bytes"
+	"context"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"regexp"
+	"strconv"
+	"syscall"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -15,7 +23,8 @@ import (
 const (
 	binName = "kavachat-api"
 
-	default_OPEN_API_KEY = "test-openai-api-key"
+	default_OPEN_API_KEY      = "test-openai-api-key"
+	default_KAVACHAT_API_PORT = 0
 )
 
 func TestMain(m *testing.M) {
@@ -33,24 +42,20 @@ func TestMain(m *testing.M) {
 	os.Exit(r)
 }
 
-func startProxy(args ...string) *exec.Cmd {
-	cmd := exec.Command(fmt.Sprintf("./%s", binName), args...)
+func startProxyCmd(t *testing.T, context context.Context, args ...string) *exec.Cmd {
+	cmd := exec.CommandContext(context, fmt.Sprintf("./%s", binName), args...)
 
-	cmd.Env = append(cmd.Env, fmt.Sprintf("OPENAI_API_KEY=%s", default_OPEN_API_KEY))
+	cmd.Env = append(cmd.Env,
+		fmt.Sprintf("OPENAI_API_KEY=%s", default_OPEN_API_KEY),
+		fmt.Sprintf("KAVACHAT_API_PORT=%d", default_KAVACHAT_API_PORT),
+	)
 
 	return cmd
 }
 
-func TestNoArgs(t *testing.T) {
-	cmd := startProxy()
-	out, err := cmd.CombinedOutput()
-
-	require.NoError(t, err, fmt.Sprintf("expected %s to not fail", cmd.String()))
-	assert.Contains(t, string(out), "level=INFO msg=\"Starting Kavachat API!\"")
-}
-
 func TestNoKeyForOpenAI(t *testing.T) {
-	cmd := startProxy()
+	ctx, _ := context.WithTimeout(context.Background(), time.Duration(1*time.Second))
+	cmd := startProxyCmd(t, ctx)
 
 	newEnv := []string{}
 	for _, envVar := range cmd.Env {
@@ -77,4 +82,95 @@ func TestNoKeyForOpenAI(t *testing.T) {
 	} else {
 		t.Fatalf("%v is not an exit error", err)
 	}
+}
+
+func TestHttpHealthCheck(t *testing.T) {
+	ctx, _ := context.WithTimeout(context.Background(), time.Duration(1*time.Second))
+	cmd := startProxyCmd(t, ctx)
+	stdout, err := cmd.StdoutPipe()
+	require.NoError(t, err, "could not obtain stdout pipe for server")
+
+	scanner := bufio.NewScanner(stdout)
+	err = cmd.Start()
+	shutdownServer := func() {
+		cmd.Process.Signal(syscall.SIGTERM)
+	}
+	defer func() {
+		// TODO: graceful shutdown and err check
+		// When exit status is non-zero, we have an ungraceful shutdown
+		// We should assert the process catches sigterm and shutdown during the
+		// test without error
+		// this should catch interrupt but not kill from deadline timeout
+		_ = cmd.Wait()
+	}()
+	require.NoError(t, err, "expected no error when starting server")
+
+	getPort, readerErr := func() (chan int, chan error) {
+		port := make(chan int)
+		readErr := make(chan error)
+
+		portMatcher, err := regexp.Compile("msg=listening port=([0-9]*)")
+		require.NoError(t, err, "invalid port regex")
+
+		go func() {
+			for scanner.Scan() {
+				line := scanner.Text()
+				matches := portMatcher.FindStringSubmatch(line)
+
+				if len(matches) > 1 {
+					parsedPort, err := strconv.Atoi(matches[1])
+					if err == nil {
+						port <- parsedPort
+					} else {
+						readErr <- err
+					}
+
+					break
+				}
+			}
+			close(port)
+		}()
+
+		return port, readErr
+	}()
+
+	port := 0
+	err = nil
+	select {
+	case port = <-getPort:
+	case err = <-readerErr:
+	case <-time.After(1 * time.Second):
+		err = errors.New("server did not start within timeout")
+	}
+	if err != nil {
+		shutdownServer()
+		require.NoError(t, err, "error when reading port")
+	}
+
+	waitUntilReady := func(ctx context.Context) chan error {
+		ready := make(chan error)
+		requestURL := fmt.Sprintf("http://localhost:%d/v1/healthcheck", port)
+		go func() {
+			for ctx.Err() == nil {
+				resp, err := http.Get(requestURL)
+				if err == nil && resp.StatusCode == 200 {
+					body, err := ioutil.ReadAll(resp.Body)
+					defer resp.Body.Close()
+					if err == nil {
+						if string(body) == "available\n" {
+							break
+						}
+					}
+				}
+			}
+			ready <- ctx.Err()
+		}()
+
+		return ready
+	}(ctx)
+
+	err = <-waitUntilReady
+	require.NoError(t, err, "server did not pass healthcheck")
+
+	shutdownServer()
 }
