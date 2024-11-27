16c4faf11f4fcf1cf09bbbe021f731d8a12d3c41
samcode206 <66322745+samcode206@users.noreply.github.com>
2024-11-27 09:18:51 -0800

    client side token generation (#6)
    * feat: install uuid library for generating session & client tokens
    
    * feat: Add client side token generation & storeage utility
    
    Adds getToken function which handles getting the tokens from local storage and session storage and building up the overall token string to be provided to the backend
    
    the session and client token are both uuid version 4 and they are seperated by a colon the overall format is {kavachat:client_token:session_token}
    
    sets up unit tests to test the token.ts file
    
    defines a general getIDFromStorage function which given a storage such as local storage or session storage it must find these tokens or generate them and store them if they don't exist this also handles the rare case where local storage or session storage don't work in which it falls back to an in-memory solution instead of crashing with an exception
    
    * feat: Add Authorization Header with Bearer token to DeepChat connection config
    
    * fix: update connection url

---
+++ src/utils/token/token.ts
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
+export const IDKEY = 'id';
+
+/**
+ * Retrieves an ID from the specified storage (localStorage or sessionStorage).
+ * If no ID exists, a new UUID is generated, stored, and returned.
+ *
+ * @param {Storage} storage - The storage object (localStorage or sessionStorage).
+ * @returns {string} The retrieved or newly generated ID.
+ */
+export function getIDFromStorage(storage: Storage) {
+    let id = '';
+    try {
+        const clientId = storage.getItem(IDKEY);
+        if (clientId) {
+            id = clientId;
+        } else {
+            id = uuidv4();
+            storage.setItem(IDKEY, id);
+        }
+    } catch (err) {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+        // https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage
+        // It is very rare but possible that localStorage or sessionStorage access fails.
+        // In that case, we fall back to an in-memory solution.
+        if (!id) id = uuidv4();
+    }
+    return id;
+}
+
+
+/**
+ * Generates or returns token by combining uuids from localStorage and sessionStorage.
+ * @returns {string} The token in the format "kavachat:{clientID}:{sessionID}".
+ */
+export function getToken() {
+    const clientToken = getIDFromStorage(window.localStorage);
+    const sessionToken = getIDFromStorage(window.sessionStorage);
+    return `kavachat:${clientToken}:${sessionToken}`;
+}
