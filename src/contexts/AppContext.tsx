2f3803d7cd515b6d5ef5f1f99e47ccdb9315c9a4
samcode206 <hadi.sameem.dev@gmail.com>
2024-11-28 13:22:41 -0800

    feat: remove deep chat package
    refactor AppContent removing all references to deep chat and reimplemnts connectWallet function

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -2,20 +2,17 @@ import {
   createContext,
   useContext,
   useState,
-  useRef,
   useCallback,
 } from "react";
-import type { ReactNode, RefObject } from "react";
-import { DeepChat } from "deep-chat";
+import type { ReactNode } from "react";
+import { useChat } from '../components/hooks/useChat';
 
 interface AppContext {
   address: string;
   connectWallet: () => Promise<void>;
-  deepChatRef: RefObject<HTMLDivElement>;
 }
 
 const initValues = {
-  deepChatRef: { current: null },
   address: "",
   connectWallet: async () => {
     throw new Error("Uninitialized");
@@ -29,44 +26,27 @@ export function AppContextProvider({
 }: {
   children: ReactNode | ReactNode[];
 }) {
-  const deepChatRef: React.RefObject<HTMLDivElement> = useRef(null);
   const [address, setAddress] = useState("");
+  const { submitChatMessage } = useChat();
 
   const connectWallet = useCallback(async () => {
-    if (deepChatRef.current == null) {
-      return;
-    }
-    // @ts-expect-error TODO: update globalThis to include ethereum
     if (!window.ethereum) {
       return;
     }
 
-    const deepChat = deepChatRef.current.children[0] as DeepChat;
-    if (!deepChat) {
-      return;
-    }
 
-    // @ts-expect-error TODO: update globalThis to include ethereum
     const accounts: string[] = await window.ethereum.request({
       method: "eth_requestAccounts",
     });
     if (accounts && accounts[0]) {
       setAddress(() => accounts[0]);
-      // a bit hacky but as per https://deepchat.dev/docs/methods they mention
-      // Make sure the Deep Chat component has been fully rendered on the DOM before using these.
-      // so we wait for the setAddress call to trigger a rerender before submitting
-      setTimeout(() => {
-        deepChat.submitUserMessage({
-          text: `connected my wallet: ${accounts[0]}`,
-        });
-      }, 50);
+      submitChatMessage(`connected my wallet: ${accounts[0]}`);
     }
   }, []);
 
   return (
     <AppContext.Provider
       value={{
-        deepChatRef,
         address,
         connectWallet,
       }}
