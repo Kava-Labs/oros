d2a2d66b12d9468e6575c5dc5909b218f7253475
samcode206 <hadi.sameem.dev@gmail.com>
2024-11-27 19:30:41 -0800

    feat: hook up redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux';
 import './index.css'
 import { App } from './App.tsx'
 import { AppContextProvider } from './contexts/AppContext.tsx'
+import { appStore } from './stores';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+    <Provider store={appStore}>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </Provider>
   </StrictMode>,
 )
