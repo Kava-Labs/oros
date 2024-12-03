3ce2b5de9990be86d158e001acc54687b21ce6d3
adamrturman <adamrturman@gmail.com>
2024-12-03 11:19:47 -0600

    fix: remove explicit redefine to strip 'VITE_' prefix

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -20,15 +20,6 @@ export default defineWorkspace([
       root: './tests',
       include: ['**/*.test.ts'],
       environment: 'node',
-    },
-    define: {
-      //  define this env variable without the VITE_ prefix
-      //  so OpenAI() can find that value without explicitly defining it each time
-      //  https://vite.dev/config/shared-options.html#envprefix
-      //  todo - helper to automate this transformation?
-      'import.meta.env.OPENAI_API_KEY': JSON.stringify(process.env.VITE_OPENAI_API_KEY),
-      'import.meta.env.PROXY_ENDPOINT': JSON.stringify(process.env.VITE_PROXY_ENDPOINT),
-      'import.meta.env.BYPASS_PROXY': JSON.stringify(process.env.VITE_BYPASS_PROXY),
     }
   }
 ])
