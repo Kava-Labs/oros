f2f54d4194295b122c42a308e20bc2543ef904f3
samcode206 <hadi.sameem.dev@gmail.com>
2024-12-03 12:13:30 -0800

    feat: add a basic playwright test to make sure the intro message is rendered

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,8 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('renders correct title', async ({ page }) => {
+test('renders intro message', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Kava Chat Bot/);
+  await page.waitForLoadState();
+
+  const messageContainer = await page.$(`[data-testid="ChatContainer"] div div`);
+  expect(messageContainer).not.toBeNull();
+
+  const role = await messageContainer!.getAttribute('data-chat-role')
+  expect(role).toBe('assistant');
+
+  // don't reference messageContainer after calling dispose
+  // https://playwright.dev/docs/api/class-elementhandle
+  // preferable we use Locators but they aren't always usable for certain elements
+  await messageContainer!.dispose();
+
+  await expect(page.getByTestId('ChatContainer')).toHaveText(/Hey I'm Kava AI/, { useInnerText: true });
+
 });
