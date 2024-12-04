aea9c8c8b51f75611161e0e218dcfdf7610acdb7
samcode206 <hadi.sameem.dev@gmail.com>
2024-12-04 12:32:11 -0800

    feat: playwright load config from either .env or .env.local

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
-import {config} from 'dotenv';
-
-config();
+import { fileURLToPath } from 'url';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: [path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.env.local'), path.resolve(process.cwd(), '.env')] });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
