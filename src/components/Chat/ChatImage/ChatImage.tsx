85f63deaa3a4476de4190d9bffb073d912853778
samcode206 <hadi.sameem.dev@gmail.com>
2024-12-10 18:45:43 -0800

    chore: prettier

--- src/components/Chat/ChatImage/ChatImage.tsx
+++ src/components/Chat/ChatImage/ChatImage.tsx
@@ -3,31 +3,30 @@ import styles from '../style.module.css';
 import { getImage } from '../../../utils/idbd/idbd';
 
 export const ChatImage = (props: { id: string }) => {
-    const { id } = props;
-    const [imgData, setImageData] = useState('');
-  
-    useEffect(() => {
-      if (!id) {
-        return;
-      }
-  
-      getImage(id)
-        .then((res) => {
-          if (res) {
-            setImageData(res.data);
-          }
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    }, [id]);
-  
-    return (
-      <div className={styles.chatBubbleAssistant}>
-        <div data-chat-role="tool" className={styles.chatBubble}>
-          <img src={`data:image/png;base64,${imgData}`} />
-        </div>
+  const { id } = props;
+  const [imgData, setImageData] = useState('');
+
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+
+    getImage(id)
+      .then((res) => {
+        if (res) {
+          setImageData(res.data);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [id]);
+
+  return (
+    <div className={styles.chatBubbleAssistant}>
+      <div data-chat-role="tool" className={styles.chatBubble}>
+        <img src={`data:image/png;base64,${imgData}`} />
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
