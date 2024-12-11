1019ded9136118465f1e9a4b155f7ac645ef01cb
Nick DeLuca <nickdeluca08@gmail.com>
2024-12-11 18:43:29 +0000

    feat: Reset base CSS for sizing and wrap fixes
    Set border-box sizing so width and heights are consistent across body
    and components -- the width and height include padding and border sizes.
    
    Set line-height for consistent text rendering and set body to a
    min-height of 100vh so background always extends the full screen.
    
    Add default overflow that allows breaking of words, which is important
    for a good chat experience and preventing text from leaving their
    containers.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
+//
+// React Imports
+//
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+//
+// Redux Imports
+//
 import { Provider } from 'react-redux';
-import './index.css';
-import { App } from './App.tsx';
-import { AppContextProvider } from './contexts/AppContext.tsx';
 import { appStore } from './stores';
+
+//
+// Global CSS & Fonts
+//
+import './global.css';
 
 import '@fontsource/inter/300.css';
 import '@fontsource/inter/300-italic.css';
@@ -18,12 +27,25 @@ import '@fontsource/inter/500-italic.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/inter/700-italic.css';
 
-createRoot(document.getElementById('root')!).render(
+//
+// Components & Context
+//
+import { App } from './App.tsx';
+import { AppContextProvider } from './contexts/AppContext.tsx';
+
+//
+// Render!
+//
+const root = createRoot(
+  document.getElementById('root')
+);
+
+root.render(
   <StrictMode>
     <Provider store={appStore}>
       <AppContextProvider>
         <App />
       </AppContextProvider>
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 );
