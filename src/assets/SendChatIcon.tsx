63799741a9e46367b4ce4a7a1168182f893076bb
Paul Downing <=>
2024-12-12 15:47:13 -0600

    chore: Move SVG icons to simple react components
    Move the reset chat and send chat icons to simple react components so
    they are extracted away from the view code and for readability

---
+++ src/assets/SendChatIcon.tsx
@@ -0,0 +1,25 @@
+export const SendChatIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      color="#FFFFFF"
+      fill="none"
+    >
+      <path
+        d="M12 4V20"
+        stroke="#FFFFFF"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4 12H20"
+        stroke="#FFFFFF"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
