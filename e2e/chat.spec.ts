e7cb1b202897cfc262973839702a1e2eae57b0ca
adamrturman <adamrturman@gmail.com>
2024-12-17 11:24:18 -0600

    fix: remove unused import

--- e2e/chat.spec.ts
+++ e2e/chat.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Locator } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { systemPrompt } from '../src/config';
 import { Chat } from './Chat';
 
