cecf1b8b6ad06515887556a0e0aa2cccdb07ae82
Paul Downing <=>
2024-12-17 01:42:50 -0600

    fix: Fix chatIcon styling on main chat page
    Since I decided to build the circular aspect directly into the svg, the
    css code that was building the circule around the chatIcon was no longer
    needed

--- src/ChatView.tsx
+++ src/ChatView.tsx
@@ -111,9 +111,7 @@ export const ChatView = ({
         {!hasMessages && (
           <div id={styles.startContainer}>
             <div id={styles.start} data-testid="start">
-              <div id={styles.chatIconContainer}>
-                <img src={hardDotFunDiamond} />
-              </div>
+              <img src={hardDotFunDiamond} id={styles.chatIconContainer} />
               <h1>Let's get started!</h1>
               <p>
                 Tell me about your memecoin idea below and we'll generate
