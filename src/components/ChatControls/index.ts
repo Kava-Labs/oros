deac4b01f28a560696e33257a720fde08a4a0048
Nick DeLuca <nickdeluca08@gmail.com>
2024-12-16 17:06:01 -0700

    chore: Remove outdated components, state, and deps (#82)
    * chore: Remove outdated components, state, and deps
    
    This removes the outdates component, stores, context, redux stuff.
    
    * fix: Remove hooks that depend on removed files and deps

--- src/components/ChatControls/index.ts
+++
@@ -1,1 +0,0 @@
-export { ChatControls } from './ChatControls';
--- src/stores/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { msgStoreReducer } from './msgStore';
-
-export const appStore = configureStore({
-  reducer: {
-    msgStore: msgStoreReducer,
-  },
-});
