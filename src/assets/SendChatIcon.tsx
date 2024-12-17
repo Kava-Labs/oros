beaa8d48cbf45062fa477910a65cfe2ca8fd73c2
Paul Downing <=>
2024-12-16 18:31:27 -0600

    feat: add launch btn and update action btn styling
    Add a "Launch" button that is not yet wired into the hard.fun demo page.
    
    Update styling for Send chat and reset chat buttons to match hard.fun

--- src/assets/SendChatIcon.tsx
+++ src/assets/SendChatIcon.tsx
@@ -1,27 +1,30 @@
 import { memo } from 'react';
 
-export const SendChatIcon = memo(() => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      color="#FFFFFF"
-      fill="none"
-    >
-      <path
-        d="M12 4V20"
-        stroke="#FFFFFF"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M4 12H20"
-        stroke="#FFFFFF"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}, () => true);
+export const SendChatIcon = memo(
+  () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        color="transparent"
+        fill="none"
+      >
+        <path
+          d="M12 4V20"
+          stroke="#ff564f"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M4 12H20"
+          stroke="#ff564f"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    );
+  },
+  () => true,
+);
