5d78b3bdb948a6bc5370a8e0e879f01c145f311b
adamrturman <adamrturman@gmail.com>
2024-12-19 14:14:10 -0600

    refactor: extract addresses to variables

--- src/utils/chat/maskAddresses.test.ts
+++ src/utils/chat/maskAddresses.test.ts
@@ -3,20 +3,18 @@ import { failureTestCases, happyPathCases } from './tokenAddressMaskingCases';
 import { expect } from '@playwright/test';
 
 describe('maskAddresses', () => {
+  const address1 = '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5';
+  const address2 = '0xC07918E451Ab77023a16Fa7515Dd60433A3c771D';
+
   test('replaces single address', () => {
     happyPathCases['singleAddress'].forEach((testCase) => {
       const { output, maskedValueMap } = maskAddresses(testCase);
 
       //  the address from the test case is removed from the output
-      expect(
-        output.includes('0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5'),
-      ).toBe(false);
+      expect(output.includes(address1)).toBe(false);
 
       //  and stored in the map
-      expect(maskedValueMap).toHaveProperty(
-        '<address_1>',
-        '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
-      );
+      expect(maskedValueMap).toHaveProperty('<address_1>', address1);
     });
   });
 
@@ -25,21 +23,11 @@ describe('maskAddresses', () => {
       const { output, maskedValueMap } = maskAddresses(testCase);
 
       //  both addresses are removed
-      expect(
-        output.includes('0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5'),
-      ).toBe(false);
-      expect(
-        output.includes('0xC07918E451Ab77023a16Fa7515Dd60433A3c771D'),
-      ).toBe(false);
+      expect(output.includes(address1)).toBe(false);
+      expect(output.includes(address2)).toBe(false);
 
-      expect(maskedValueMap).toHaveProperty(
-        '<address_1>',
-        '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
-      );
-      expect(maskedValueMap).toHaveProperty(
-        '<address_2>',
-        '0xC07918E451Ab77023a16Fa7515Dd60433A3c771D',
-      );
+      expect(maskedValueMap).toHaveProperty('<address_1>', address1);
+      expect(maskedValueMap).toHaveProperty('<address_2>', address2);
     });
   });
 
