11de40ea44302670901ebf12035d138c90708a9e
adamrturman <adamrturman@gmail.com>
2024-12-20 13:42:41 -0600

    feat: include reverse map of values to masks for quicker lookup of existing

--- src/utils/chat/maskAddresses.test.ts
+++ src/utils/chat/maskAddresses.test.ts
@@ -4,7 +4,8 @@ interface MaskedAddressTestCase {
   input: string;
   output: {
     result: string;
-    maskedValueMap: Record<string, string>;
+    masksToValues: Record<string, string>;
+    valuesToMasks: Record<string, string>;
   };
 }
 
@@ -15,8 +16,11 @@ describe('maskAddresses', () => {
         input: 'Send 100 KAVA to 0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
         output: {
           result: 'Send 100 KAVA to <address_1>',
-          maskedValueMap: {
+          masksToValues: {
             address_1: '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
+          },
+          valuesToMasks: {
+            '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5': 'address_1',
           },
         },
       },
@@ -24,8 +28,11 @@ describe('maskAddresses', () => {
         input: 'Send 100 KAVA to 0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5!',
         output: {
           result: 'Send 100 KAVA to <address_1>!',
-          maskedValueMap: {
+          masksToValues: {
             address_1: '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
+          },
+          valuesToMasks: {
+            '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5': 'address_1',
           },
         },
       },
@@ -33,8 +40,11 @@ describe('maskAddresses', () => {
         input: 'Send 100 KAVA to0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
         output: {
           result: 'Send 100 KAVA to<address_1>',
-          maskedValueMap: {
+          masksToValues: {
             address_1: '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
+          },
+          valuesToMasks: {
+            '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5': 'address_1',
           },
         },
       },
@@ -43,8 +53,11 @@ describe('maskAddresses', () => {
           'Send 100 KAVA and 0.01 wBTC to 0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
         output: {
           result: 'Send 100 KAVA and 0.01 wBTC to <address_1>',
-          maskedValueMap: {
+          masksToValues: {
             address_1: '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
+          },
+          valuesToMasks: {
+            '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5': 'address_1',
           },
         },
       },
@@ -53,8 +66,11 @@ describe('maskAddresses', () => {
           'Send 100 KAVA to 0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5 and 0.01 wBTC to 0xd8e30f7bcb5211e591bbc463cdab0144e82dFFE5',
         output: {
           result: 'Send 100 KAVA to <address_1> and 0.01 wBTC to <address_1>',
-          maskedValueMap: {
+          masksToValues: {
             address_1: '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
+          },
+          valuesToMasks: {
+            '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5': 'address_1',
           },
         },
       },
@@ -63,9 +79,13 @@ describe('maskAddresses', () => {
           'Send 100 KAVA to 0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5 and 0.01 wBTC to 0xC07918E451Ab77023a16Fa7515Dd60433A3c771D',
         output: {
           result: 'Send 100 KAVA to <address_1> and 0.01 wBTC to <address_2>',
-          maskedValueMap: {
+          masksToValues: {
             address_1: '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
             address_2: '0xc07918e451ab77023a16fa7515dd60433a3c771d',
+          },
+          valuesToMasks: {
+            '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5': 'address_1',
+            '0xc07918e451ab77023a16fa7515dd60433a3c771d': 'address_2',
           },
         },
       },
@@ -75,21 +95,30 @@ describe('maskAddresses', () => {
         output: {
           result:
             'Send 10 KAVA to the following addresses: <address_1>, <address_2>, <address_3>, <address_4>',
-          maskedValueMap: {
+          masksToValues: {
             address_1: '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5',
             address_2: '0xc07918e451ab77023a16fa7515dd60433a3c771d',
             address_3: '0x7bbf300890857b8c241b219c6a489431669b3afa',
             address_4: '0x1874c3e9d6e5f7e4f3f22c3e260c8b25ed1433f2',
+          },
+          valuesToMasks: {
+            '0xd8e30f7bcb5211e591bbc463cdab0144e82dffe5': 'address_1',
+            '0xc07918e451ab77023a16fa7515dd60433a3c771d': 'address_2',
+            '0x7bbf300890857b8c241b219c6a489431669b3afa': 'address_3',
+            '0x1874c3e9d6e5f7e4f3f22c3e260c8b25ed1433f2': 'address_4',
           },
         },
       },
     ];
 
     testCases.forEach((testCase) => {
-      const { output, maskedValueMap } = maskAddresses(testCase.input);
+      const { output, masksToValues, valuesToMasks } = maskAddresses(
+        testCase.input,
+      );
 
       expect(output).toBe(testCase.output.result);
-      expect(maskedValueMap).toStrictEqual(testCase.output.maskedValueMap);
+      expect(masksToValues).toStrictEqual(testCase.output.masksToValues);
+      expect(valuesToMasks).toStrictEqual(testCase.output.valuesToMasks);
     });
   });
 
@@ -100,14 +129,16 @@ describe('maskAddresses', () => {
         result: {
           output:
             'Send 100 KAVA to kava1vlpsrmdyuywvaqrv7rx6xga224sqfwz3fyfhwq',
-          maskedValueMap: {},
+          masksToValues: {},
+          valuesToMasks: {},
         },
       },
       {
         input: 'Send 100 KAVA to Binance',
         result: {
           output: 'Send 100 KAVA to Binance',
-          maskedValueMap: {},
+          masksToValues: {},
+          valuesToMasks: {},
         },
       },
       {
@@ -116,16 +147,20 @@ describe('maskAddresses', () => {
         result: {
           output:
             'Send 100 KAVA to 0xd8e30f7bcb5211e591bbc463cdab0144e82dffe500000000000000000000000',
-          maskedValueMap: {},
+          masksToValues: {},
+          valuesToMasks: {},
         },
       },
     ];
 
     testCases.forEach((testCase) => {
-      const { output, maskedValueMap } = maskAddresses(testCase.input);
+      const { output, masksToValues, valuesToMasks } = maskAddresses(
+        testCase.input,
+      );
 
       expect(output).toBe(testCase.input);
-      expect(maskedValueMap).toStrictEqual({});
+      expect(masksToValues).toStrictEqual({});
+      expect(valuesToMasks).toStrictEqual({});
     });
   });
 });
--- src/utils/chat/maskAddresses.ts
+++ src/utils/chat/maskAddresses.ts
@@ -1,6 +1,7 @@
 type AddressAccumulator = {
   updatedResult: string;
-  maskedValueMap: Record<string, string>;
+  masksToValues: Record<string, string>;
+  valuesToMasks: Record<string, string>;
 };
 /**
  * Masks unique addresses in a given message with placeholders (e.g., <address_1>, <address_2>, etc.).
@@ -17,30 +18,31 @@ export const maskAddresses = (message: string) => {
 
   const addressesToReplace = Array.from(message.matchAll(ethAddressRegex));
 
-  const { updatedResult, maskedValueMap } =
+  const { updatedResult, masksToValues, valuesToMasks } =
     addressesToReplace.reduce<AddressAccumulator>(
       (accumulator, addressMatch) => {
         // Convert the matched address to lowercase
         const address = addressMatch[0].toLowerCase();
 
-        // Retrieve existing keys from the maskedValueMap
-        const existingEntries = Object.keys(accumulator.maskedValueMap);
+        // Retrieve existing keys from the masksToValues map
+        const existingEntries = Object.keys(accumulator.masksToValues);
 
-        // Check if the address is already in the map
+        // Check if the address is already in the masksToValues map
         let key = existingEntries.find(
-          (existingKey) => accumulator.maskedValueMap[existingKey] === address,
+          (existingKey) => accumulator.masksToValues[existingKey] === address,
         );
 
         // If not, create a new key for this address
         if (!key) {
           key = `address_${existingEntries.length + 1}`;
-          accumulator.maskedValueMap[key] = address; // Add lowercased address
+          accumulator.masksToValues[key] = address; // Add lowercased address
+          accumulator.valuesToMasks[address] = key; // Create reverse mapping
         }
 
         // Create a replacement string with the key
         const replacement = `<${key}>`;
 
-        // Replace all occurrences of the (lowercase) address in the result
+        // Replace all occurrences of the address in the result
         const regex = new RegExp(address, 'gi');
         accumulator.updatedResult = accumulator.updatedResult.replace(
           regex,
@@ -51,12 +53,14 @@ export const maskAddresses = (message: string) => {
       },
       {
         updatedResult: message,
-        maskedValueMap: {},
+        masksToValues: {},
+        valuesToMasks: {},
       },
     );
 
   return {
     output: updatedResult,
-    maskedValueMap,
+    masksToValues,
+    valuesToMasks,
   };
 };
