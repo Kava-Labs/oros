75df83664939484e62be1cfdb2188d511cf96f7f
adamrturman <adamrturman@gmail.com>
2025-01-14 13:42:17 -0600

    fix: wrap test in theme provider

--- src/ChatView.test.tsx
+++ src/ChatView.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import { ChatView, ChatViewProps } from './ChatView';
+import { ThemeProvider } from './theme/themeProvider';
 
 describe('ChatView', () => {
   const props: ChatViewProps = {
@@ -13,11 +14,14 @@ describe('ChatView', () => {
     address: '',
     chainID: '2222',
   };
+
   test('input is focused by default', () => {
-    const wrapper = render(<ChatView {...props} />);
-
+    const wrapper = render(
+      <ThemeProvider>
+        <ChatView {...props} />
+      </ThemeProvider>,
+    );
     const input = wrapper.getByTestId('chat-view-input');
-
     expect(input).toHaveFocus();
   });
 });
