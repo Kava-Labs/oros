3773ccc5d123bffcf65d1cbbe56f698d8504f48d
samcode206 <hadi.sameem.dev@gmail.com>
2025-01-23 11:19:45 -0800

    fix: wrap Chatview and App context in wallet provider

--- src/components/ChatView.test.tsx
+++ src/components/ChatView.test.tsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 import { ChatView, ChatViewProps } from './ChatView';
 import { ThemeProvider } from '../theme/themeProvider';
 import { AppContextProvider } from '../context/AppContextProvider';
+import { WalletContextProvider } from '../context/WalletContextProvider';
 
 describe('ChatView', () => {
   const props: ChatViewProps = {
@@ -16,9 +17,11 @@ describe('ChatView', () => {
   test('input is focused by default', () => {
     const wrapper = render(
       <ThemeProvider>
-        <AppContextProvider>
-          <ChatView {...props} />
-        </AppContextProvider>
+        <WalletContextProvider>
+          <AppContextProvider>
+            <ChatView {...props} />
+          </AppContextProvider>
+        </WalletContextProvider>
       </ThemeProvider>,
     );
     const input = wrapper.getByTestId('chat-view-input');
