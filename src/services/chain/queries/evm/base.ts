4782f718acf96c8c086f154fa58d5741a18f5ed3
Adam Robert Turman <67024033+adamrturman@users.noreply.github.com>
2025-01-22 21:20:04 -0600

    feat: evm balances query (#167)
    * feat: fetch evm balance
    
    * refactor: split into directories
    
    * fix: adjust imports

---
+++ src/services/chain/queries/evm/base.ts
@@ -0,0 +1,15 @@
+import { MessageParam } from '../../../../types/messages';
+import { ChainOperation } from '../../../../types/chain';
+
+export abstract class QueryBase<T> implements ChainOperation {
+  abstract name: string;
+  operationType = 'query' as const;
+  chainType = 'evm' as const;
+
+  abstract description: string;
+  abstract parameters: Array<MessageParam>;
+
+  abstract validate(params: T): boolean;
+
+  abstract executeQuery(params: T): Promise<string>;
+}
