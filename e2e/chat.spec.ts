f78eab82668f5b13ba95682799e552a58047605a
Paul Downing <=>
2025-02-11 12:40:27 -0700

    fix: revert e2e test changes for mofrl name

--- e2e/chat.spec.ts
+++ e2e/chat.spec.ts
@@ -352,11 +352,11 @@ describe('chat', () => {
     await chat.goto();
 
     const historyEntry = page.getByTestId('chat-history-entry').first();
-    await expect(historyEntry).toHaveText('Test Conversation Titletest-model');
+    await expect(historyEntry).toHaveText('Test Conversation Title');
 
     //  still there after a refresh/reload
     await page.reload();
 
-    await expect(historyEntry).toHaveText('Test Conversation Titletest-model');
+    await expect(historyEntry).toHaveText('Test Conversation Titletest');
   });
 });
