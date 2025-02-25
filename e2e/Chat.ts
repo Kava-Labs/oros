52889ef4ab3e8da66c87d9c67b0c203be7fbb75f
adamrturman <adamrturman@gmail.com>
2025-02-25 16:29:47 -0600

    fix: remove unused var

--- e2e/Chat.ts
+++ e2e/Chat.ts
@@ -1,4 +1,4 @@
-import { ElementHandle, expect, Locator, Page } from '@playwright/test';
+import { ElementHandle, Locator, Page } from '@playwright/test';
 
 export class Chat {
   public page: Page;
