fff5b3571c58d47fb2510b593fafd6ec990ee9a9
adamrturman <adamrturman@gmail.com>
2025-03-04 15:55:56 -0600

    refactor: reorganize

--- src/features/blockchain/utils/wallet.ts
+++ src/features/blockchain/utils/wallet.ts
@@ -1,23 +1,7 @@
 import { WalletStore, WalletTypes } from '../stores/walletStore';
 import { ChainType } from '../types/chain';
 import { chainRegistry } from '../config/chainsRegistry';
-
-/**
- * Detects if the current device is a mobile device based on user agent and viewport width
- * @returns boolean - true if the device is mobile, false otherwise
- */
-export function isMobileDevice(): boolean {
-  //  Use regex to detect mobile
-  const isMobileUserAgent =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      window.navigator.userAgent,
-    );
-
-  const isMobileViewport = window.innerWidth <= 768;
-
-  //  But also confirm the viewport (chrome devtools throws a false positive based on agent-alone)
-  return isMobileUserAgent && isMobileViewport;
-}
+import { isMobileDevice } from '../../../core/utils/isMobileDevice';
 
 /**
  * Validates if the wallet is connected and compatible
