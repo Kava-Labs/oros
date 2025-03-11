09795661a3f850dec8b00931253d747e335077ff
adamrturman <adamrturman@gmail.com>
2025-03-11 15:59:05 -0500

    feat: replace deepseek

--- src/features/reasoning/config/models.ts
+++ src/features/reasoning/config/models.ts
@@ -10,7 +10,7 @@ import {
 import KavaIcon from '../../../core/assets/KavaIcon';
 import { calculateFinalChunkTokenUsage } from '../helpers';
 
-const supportedReasoningModels = ['deepseek-r1', 'qwq-32b-bnb-4bit'] as const;
+const supportedReasoningModels = ['qwq-32b-bnb-4bit'] as const;
 
 export type SupportedReasoningModel = (typeof supportedReasoningModels)[number];
 
@@ -24,25 +24,6 @@ export const reasoningModels: Record<
   SupportedReasoningModel,
   ReasoningModelConfig
 > = {
-  'deepseek-r1': {
-    id: 'deepseek-r1',
-    name: 'General Reasoning',
-    icon: KavaIcon,
-    description: 'Logical Analysis',
-    includeUsageInStream: false,
-    tools: [],
-    supportedFileTypes: ['image/jpeg', 'image/png', 'image/webp'],
-    maximumFileUploads: 4,
-    maximumFileBytes: 8 * 1024 * 1024, //  8MB
-    //  not running full 128K token context currently
-    contextLength: 8192,
-    contextLimitMonitor: calculateFinalChunkTokenUsage,
-    contextWarningThresholdPercentage: 5,
-    conversationResetTokenThreshold: 100,
-    systemPrompt: defaultSystemPrompt,
-    introText: defaultIntroText,
-    inputPlaceholderText: defaultInputPlaceholderText,
-  },
   'qwq-32b-bnb-4bit': {
     id: 'qwq-32b-bnb-4bit',
     name: 'General Reasoning',
