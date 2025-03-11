12ed478fc07dec2c6df2bbd3a4785dfa7f73f68d
adamrturman <adamrturman@gmail.com>
2025-03-11 14:45:02 -0500

    refactor: extensible rendering of thinking content

--- src/core/config/models.ts
+++ src/core/config/models.ts
@@ -2,7 +2,10 @@ import {
   blockchainModels,
   SupportedBlockchainModels,
 } from '../../features/blockchain/config/models';
-import { reasoningModels } from '../../features/reasoning/config/models';
+import {
+  reasoningModels,
+  SupportedReasoningModels,
+} from '../../features/reasoning/config/models';
 import { ModelConfig, ModelRegistry, SupportedModels } from '../types/models';
 
 export const MODEL_REGISTRY: ModelRegistry = {
@@ -14,6 +17,12 @@ export const isBlockchainModelName = (
   name: SupportedModels,
 ): name is SupportedBlockchainModels => {
   return Object.keys(MODEL_REGISTRY.blockchain).includes(name);
+};
+
+export const isReasoningModel = (
+  id: string,
+): id is SupportedReasoningModels => {
+  return Object.keys(MODEL_REGISTRY.reasoning).includes(id);
 };
 
 export const getModelByName = (
