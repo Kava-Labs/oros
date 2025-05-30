ae66800217e5f38f6c420c43e82831ddca836d95
Robert Pirtle <Astropirtle@gmail.com>
2025-05-30 09:08:33 -0700

    feat: switchover backend from custom to litellm (#544)

--- src/core/utils/token/token.ts
+++ src/core/utils/token/token.ts
@@ -2,6 +2,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const IDKEY = 'id';
 
+export const USE_LITELLM_TOKEN =
+  import.meta.env.VITE_FEAT_USE_LITELLM_TOKEN === 'true';
+
 /**
  * Retrieves an ID from the specified storage (localStorage or sessionStorage).
  * If no ID exists, a new UUID is generated, stored, and returned.
@@ -35,6 +38,13 @@ export function getIDFromStorage(storage: Storage) {
  * @returns {string} The token in the format "kavachat:{clientID}:{sessionID}".
  */
 export function getToken() {
+  if (USE_LITELLM_TOKEN) {
+    // using LiteLLM service account token here is no different from a security standpoint
+    // form having an unauthenticated endpoint. the benefit here is we could rotate the key
+    // on deployment. When the need arises, we can implement custom auth middleware and fetch
+    // the token here.
+    return import.meta.env.VITE_LITELLM_API_KEY;
+  }
   const clientToken = getIDFromStorage(window.localStorage);
   const sessionToken = getIDFromStorage(window.sessionStorage);
   return `kavachat:${clientToken}:${sessionToken}`;
